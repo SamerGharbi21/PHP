7632d836f4558a54445f74ee51654870113eeb19
SamerGharbi21 <Gharbisamer2@gmail.com>
2023-10-23 01:53:57 +0300

    init

--- cal.php
+++ cal.php
@@ -1,13 +1,27 @@
 <?php 
 
-$result = $_POST["Number1"] + $_POST["Number2"];
+$number1 = $_GET["Number1"];
+$number2 = $_GET["Number2"];
+$op = $_GET["op"];
 
+if(!empty($number1) && !empty($number2 && !empty($op))){
+if($op === "+"){
+$result = $_GET["Number1"] + $_GET["Number2"];
+}elseif($op === "-"){
+    $result = $_GET["Number1"] - $_GET["Number2"];
+}elseif($op === "*"){
+    $result = $_GET["Number1"] * $_GET["Number2"];
+}elseif($op === "/"){
+    $result = $_GET["Number1"] /$_GET["Number2"];
+} 
+}else{
+    $result = "wrong input !!";
+}
 ?>
 
 
 <head>
-<meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
@@ -16,7 +30,7 @@ $result = $_POST["Number1"] + $_POST["Number2"];
     <br>
     <div class="container">
 
-    <form action="cal.php" method="POST" class="form">
+    <form action="cal.php" method="GET" class="form">
         <div>
             <label> Enter Number 1 </label>
             <input type="number" name="Number1" class="form-control">
@@ -30,13 +44,19 @@ $result = $_POST["Number1"] + $_POST["Number2"];
             <input type="number" name="Number2" class="form-control">
 </div>
 <br>
-<div  class="alert alert-success">
+<div>
+    <label> Opration </label>
+    <input type="text" name="op" class="form-control">
+</div>
+    <br>
+    <div  class="alert alert-success">
     <?php
     echo $result
     ?>
     </div>
-
+<br>
     <input type="submit" class="btn btn-primary">
+   
 </form>
 </div>
 </body>
